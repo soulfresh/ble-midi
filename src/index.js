8e8658b059408d875bc11e13f6446dbd502aa348
Robert Marc Wren <>
2018-08-22 15:39:01 -0600

    Added ParsePacket to the exported modules.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as createPeripheral } from './peripheral/create-peripheral';
 export { default as listDevices } from './midi/list-devices';
+export { default as parsePacket } from './packets/parse-packet';
